9ca5c343e571717fc6508bcbabfd93188b80d47c
Bensouda Rachid <bensourachid@gmail.com>
2016-05-25 17:01:03 +0100

    hello world project setup phase

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import HelloWorld from './hello-world';
+
+ReactDom.render(
+  <HelloWorld />,
+  document.getElementById('root')
+);
