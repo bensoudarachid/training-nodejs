d802792948cb13bba9123e7eae34b80030ea4f2e
Bensouda Rachid <bensourachid@gmail.com>
2017-05-05 11:26:35 +0100

    test build and push script 3

--- src/app/navigation/navpublic.js
+++ src/app/navigation/navpublic.js
@@ -117,7 +117,7 @@ class NavPublic extends Component {
 	</ul>
 		<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul className="nav navbar-nav navbar-left">
-			<li><IndexLink activeClassName='active' to='/'>Casa</IndexLink></li>
+			<li><IndexLink activeClassName='active' to='/'>LaCasa</IndexLink></li>
       <li><Link activeClassName='active' to='/trainings'>Training</Link></li>
 			</ul>
 			<ul className="nav navbar-nav navbar-right">
