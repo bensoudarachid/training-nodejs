e1bc6226ffa662072040dab522a4deda41d793d6
Bensouda Rachid <bensourachid@gmail.com>
2017-05-06 20:53:53 +0100

    push deploy whole app 18

--- build/app/navigation/navpublic.js
+++ build/app/navigation/navpublic.js
@@ -192,7 +192,7 @@ var NavPublic = function (_Component) {
 							_react2.default.createElement(
 								_reactRouter.IndexLink,
 								{ activeClassName: 'active', to: '/' },
-								'NoJMX'
+								'Tshash'
 							)
 						),
 						_react2.default.createElement(
