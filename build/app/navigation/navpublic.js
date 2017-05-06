11e698d94b86f8201a05689fcd7e7141457e6513
Bensouda Rachid <bensourachid@gmail.com>
2017-05-06 15:48:29 +0100

    # This is a combination of 3 commits. # The first commit's message is: push deploy whole app 17
    # The 2nd commit message will be skipped:
    
    #	push deploy whole app 18
    
    # The 3rd commit message will be skipped:
    
    #	push deploy whole app 19

--- build/app/navigation/navpublic.js
+++ build/app/navigation/navpublic.js
@@ -192,7 +192,7 @@ var NavPublic = function (_Component) {
 							_react2.default.createElement(
 								_reactRouter.IndexLink,
 								{ activeClassName: 'active', to: '/' },
-								'Home'
+								'Thread'
 							)
 						),
 						_react2.default.createElement(
