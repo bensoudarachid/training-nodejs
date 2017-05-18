82b87783c193eb5f120567663040ee7eeb35492a
Bensouda Rachid <bensourachid@gmail.com>
2017-05-18 23:58:59 +0100

    test push deploy whole app. Now with pm2 and tomcat cluster. test pm2 reload. 2

--- build/services/version.js
+++ build/services/version.js
@@ -3,4 +3,4 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var appversion = exports.appversion = '2017-05-18-21-16-27-1627';
+var appversion = exports.appversion = '2017-05-18-23-56-57-5657';
