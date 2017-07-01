61a572c15b86d5c0c25fc8737c9213b4ef7561bf
Bensouda Rachid <bensourachid@gmail.com>
2017-07-01 09:43:58 +0000

    Test certificate renew on production. do it on training save 3

--- build/services/version.js
+++ build/services/version.js
@@ -3,4 +3,4 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var appversion = exports.appversion = '2017-07-01-09-32-15-3215';
+var appversion = exports.appversion = '2017-07-01-09-41-15-4115';
