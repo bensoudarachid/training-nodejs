7e839697ca070b818aa205333ad96fc8f2cebfbd
Bensouda Rachid <bensourachid@gmail.com>
2018-04-21 19:53:11 +0100

    prepare an openshift working version without rabbitmq

--- build/scenes/training/public/trainingapp.js
+++ build/scenes/training/public/trainingapp.js
@@ -34,7 +34,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 // require('stompjs')
 // import SockJS from 'sockjs-client'
-var StompClient = require('stompjs').StompClient;
+// var StompClient = require('stompjs').StompClient
 
 if (typeof require.ensure !== 'function') require.ensure = function (d, c) {
     return c(require);
@@ -51,9 +51,9 @@ var TrainingApp = function (_Component) {
         _classCallCheck(this, TrainingApp);
 
         var eventSource;
-        var stompClient = undefined;
-        var websocket = undefined;
-        var stompSubscription = undefined;
+        // var stompClient = undefined
+        // var websocket = undefined
+        // var stompSubscription = undefined
 
         var _this = _possibleConstructorReturn(this, (TrainingApp.__proto__ || Object.getPrototypeOf(TrainingApp)).call(this, props));
 
@@ -110,57 +110,55 @@ var TrainingApp = function (_Component) {
             window.switchTextArray = ['Java', 'Javascript', 'Spring Boot', 'Spring Security', 'Rest', 'Agile', 'Ooa', 'Ood', 'System Security', 'Sound Edition', 'Web-Design', 'E-Commerce', 'React', 'Html5', 'Css3', 'Virtualization', 'Flat design', 'Cloud', 'Angular', 'Json', 'Xml', 'Sql', 'Mysql', 'Hibernate', 'JPA', 'Webpack', 'Node.js', 'Git', 'Code Versioning', 'UML', 'Eclipse', 'Design Pattern', 'Music production', 'Sass'];
             this.textswitcher();
             if (process.env.BROWSER) {
-                // let socketUrl = 'http://192.168.99.100:15674/stomp'
-                var socketUrl = 'http://192.168.99.100:31674/stomp';
-                var successCallback = function () {
-                    this.stompClient.send('/queue/jsa.sendqueue', {}, JSON.stringify({ 'time': new Date().toTimeString(), 'content': window.location.hostname }));
-                    this.stompSubscription = this.stompClient.subscribe('/exchange/amq.topic/jsa.routingkey.abbaslearn', function (data) {
-                        var body = data.body;
-                        console.log('body=' + require('util').inspect(body, false, null));
-                        var message = JSON.parse(body);
-                        console.log('message=' + require('util').inspect(message.time, false, null));
-                        (0, _jquery2.default)('#timestamp') //.text(message.time)
-                        .shuffleLetters({
-                            'text': message.time
-                        });
-                    }, {
-                        'durable': true,
-                        'auto-delete': true
-                    });
-                }.bind(this);
+                // let socketUrl = 'http://192.168.99.100:31674/stomp'
+                // let successCallback = function () {
+                //     this.stompClient.send('/queue/jsa.sendqueue', {}, JSON.stringify({'time':new Date().toTimeString(),'content':window.location.hostname}))
+                //     this.stompSubscription = this.stompClient.subscribe('/exchange/amq.topic/jsa.routingkey.abbaslearn', function (data) {
+                //         var body = data.body
+                //         console.log('body='+require('util').inspect(body, false, null))
+                //         var message = JSON.parse(body)
+                //         console.log('message=' + require('util').inspect(message.time, false, null))
+                //         $('#timestamp')//.text(message.time)
+                //             .shuffleLetters({
+                //                 'text': message.time
+                //             })
+                //     }, {
+                //         'durable': true,
+                //         'auto-delete': true
+                //     })
+                // }.bind(this)
 
-                var connectAndReconnect = function (socketUrl, successCallback) {
-                    if (this.stompClient != undefined) this.stompClient.disconnect();
-                    if (this.websocket != undefined) this.websocket.close();
-                    this.websocket = new SockJS(socketUrl, null, { protocols_whitelist: ['xdr-streaming', 'xhr-streaming', 'iframe-eventsource', 'iframe-htmlfile', 'xdr-polling', 'xhr-polling', 'iframe-xhr-polling', 'jsonp-polling'] });
-                    this.stompClient = Stomp.over(this.websocket);
-                    this.stompClient.heartbeat.outgoing = 0;
-                    this.stompClient.heartbeat.incoming = 0;
-                    // this.stompClient = Stomp.client(socketUrl)
-                    this.stompClient.connect('guest', 'guest', successCallback, function () {
-                        console.log('Oops! Reconnect');
-                        setTimeout(function () {
-                            connectAndReconnect(socketUrl, successCallback);
-                        }, 4000);
-                    }, '/');
-                }.bind(this);
-                connectAndReconnect(socketUrl, successCallback);
+                // let connectAndReconnect = function (socketUrl, successCallback) {
+                //     if (this.stompClient!=undefined)
+                //         this.stompClient.disconnect()
+                //     if (this.websocket !=undefined)
+                //         this.websocket.close()
+                //     this.websocket =  new SockJS(socketUrl, null, { protocols_whitelist: ['xdr-streaming', 'xhr-streaming', 'iframe-eventsource', 'iframe-htmlfile', 'xdr-polling', 'xhr-polling', 'iframe-xhr-polling', 'jsonp-polling'] })
+                //     this.stompClient = Stomp.over(this.websocket)
+                //     this.stompClient.heartbeat.outgoing = 0
+                //     this.stompClient.heartbeat.incoming = 0
+                //     // this.stompClient = Stomp.client(socketUrl)
+                //     this.stompClient.connect(
+                //         'guest',
+                //         'guest',
+                //         successCallback,
+                //         () => {
+                //             console.log('Oops! Reconnect')
+                //             setTimeout(() => {
+                //                 connectAndReconnect(socketUrl, successCallback)
+                //             }, 4000)
+                //         },
+                //         '/'
+                //     );
+                //
+                // }.bind(this)
+                // connectAndReconnect(socketUrl, successCallback)
             }
         }
     }, {
         key: 'sendName',
         value: function sendName(name) {
             console.log('Send');
-            // this.stompClient.send("/api/guestbook", {}, "Wow")
-            // this.stompClient.send('/queue/jsa.sendqueue', {'reply-to': '/temp-queue/foo'}, 'Wow')
-            // this.stompClient.send('/queue/jsa.sendqueue', {"content-type":"text/plain"}, 'Hello, STOMP')
-            // this.stompClient.send('/queue/jsa.sendqueue', {}, 'Hello, STOMP')
-            // 'Content-Type': 'application/json'
-            // text/html; charset=utf-8
-            this.stompClient.send('/queue/jsa.sendqueue', {}, JSON.stringify({ 'time': new Date().toTimeString(), 'content': window.location.hostname }));
-            // this.stompClient.send('/queue/j  sa.sendqueue', {'reply-to': '/temp-queue/foo'}, {'time':new Date().toTimeString(),'content':'payload'})
-            // this.stompClient.send('/exchange/amq.direct/tenantname', {'reply-to': '/temp-queue/foo'}, 'Wow '+new Date().toTimeString())
-            // this.stompClient.send("/queue/jsa.queue", {}, "Wow")
         }
     }, {
         key: 'componentDidUpdate',
@@ -171,8 +169,10 @@ var TrainingApp = function (_Component) {
             console.log('closing eventSource !');
             this.isTextSwitchAnimated = false;
             // this.eventSource.close()
-            if (this.stompSubscription != undefined) this.stompSubscription.unsubscribe();
-            if (this.stompClient != undefined) this.stompClient.disconnect();
+            // if(this.stompSubscription!=undefined )
+            //     this.stompSubscription.unsubscribe()
+            // if(this.stompClient!=undefined )
+            //     this.stompClient.disconnect()
             // this.websocket.disconnect()
         }
     }, {
--- src/scenes/training/public/trainingapp.js
+++ src/scenes/training/public/trainingapp.js
@@ -6,7 +6,7 @@ import './trainingapp.scss'
 
 // require('stompjs')
 // import SockJS from 'sockjs-client'
-var StompClient = require('stompjs').StompClient
+// var StompClient = require('stompjs').StompClient
 
 if (typeof require.ensure !== 'function') require.ensure = function (d, c) {
     return c(require)
@@ -20,9 +20,9 @@ class TrainingApp extends Component {
 
     constructor(props) {
         var eventSource
-        var stompClient = undefined
-        var websocket = undefined
-        var stompSubscription = undefined
+        // var stompClient = undefined
+        // var websocket = undefined
+        // var stompSubscription = undefined
         super(props)
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
         this.isTextSwitchAnimated = false
@@ -70,65 +70,54 @@ class TrainingApp extends Component {
         window.switchTextArray = ['Java', 'Javascript', 'Spring Boot', 'Spring Security', 'Rest', 'Agile', 'Ooa', 'Ood', 'System Security', 'Sound Edition', 'Web-Design', 'E-Commerce', 'React', 'Html5', 'Css3', 'Virtualization', 'Flat design', 'Cloud', 'Angular', 'Json', 'Xml', 'Sql', 'Mysql', 'Hibernate', 'JPA', 'Webpack', 'Node.js', 'Git', 'Code Versioning', 'UML', 'Eclipse', 'Design Pattern', 'Music production', 'Sass']
         this.textswitcher()
         if (process.env.BROWSER) {
-            // let socketUrl = 'http://192.168.99.100:15674/stomp'
-            let socketUrl = 'http://192.168.99.100:31674/stomp'
-            let successCallback = function () {
-                this.stompClient.send('/queue/jsa.sendqueue', {}, JSON.stringify({'time':new Date().toTimeString(),'content':window.location.hostname}))
-                this.stompSubscription = this.stompClient.subscribe('/exchange/amq.topic/jsa.routingkey.abbaslearn', function (data) {
-                    var body = data.body
-                    console.log('body='+require('util').inspect(body, false, null))
-                    var message = JSON.parse(body)
-                    console.log('message=' + require('util').inspect(message.time, false, null))
-                    $('#timestamp')//.text(message.time)
-                        .shuffleLetters({
-                            'text': message.time
-                        })
-                }, {
-                    'durable': true,
-                    'auto-delete': true
-                })
-            }.bind(this)
+            // let socketUrl = 'http://192.168.99.100:31674/stomp'
+            // let successCallback = function () {
+            //     this.stompClient.send('/queue/jsa.sendqueue', {}, JSON.stringify({'time':new Date().toTimeString(),'content':window.location.hostname}))
+            //     this.stompSubscription = this.stompClient.subscribe('/exchange/amq.topic/jsa.routingkey.abbaslearn', function (data) {
+            //         var body = data.body
+            //         console.log('body='+require('util').inspect(body, false, null))
+            //         var message = JSON.parse(body)
+            //         console.log('message=' + require('util').inspect(message.time, false, null))
+            //         $('#timestamp')//.text(message.time)
+            //             .shuffleLetters({
+            //                 'text': message.time
+            //             })
+            //     }, {
+            //         'durable': true,
+            //         'auto-delete': true
+            //     })
+            // }.bind(this)
 
-            let connectAndReconnect = function (socketUrl, successCallback) {
-                if (this.stompClient!=undefined)
-                    this.stompClient.disconnect()
-                if (this.websocket !=undefined)
-                    this.websocket.close()
-                this.websocket =  new SockJS(socketUrl, null, { protocols_whitelist: ['xdr-streaming', 'xhr-streaming', 'iframe-eventsource', 'iframe-htmlfile', 'xdr-polling', 'xhr-polling', 'iframe-xhr-polling', 'jsonp-polling'] })
-                this.stompClient = Stomp.over(this.websocket)
-                this.stompClient.heartbeat.outgoing = 0
-                this.stompClient.heartbeat.incoming = 0
-                // this.stompClient = Stomp.client(socketUrl)
-                this.stompClient.connect(
-                    'guest',
-                    'guest',
-                    successCallback,
-                    () => {
-                        console.log('Oops! Reconnect')
-                        setTimeout(() => {
-                            connectAndReconnect(socketUrl, successCallback)
-                        }, 4000)
-                    },
-                    '/'
-                );
-
-            }.bind(this)
-            connectAndReconnect(socketUrl, successCallback)
+            // let connectAndReconnect = function (socketUrl, successCallback) {
+            //     if (this.stompClient!=undefined)
+            //         this.stompClient.disconnect()
+            //     if (this.websocket !=undefined)
+            //         this.websocket.close()
+            //     this.websocket =  new SockJS(socketUrl, null, { protocols_whitelist: ['xdr-streaming', 'xhr-streaming', 'iframe-eventsource', 'iframe-htmlfile', 'xdr-polling', 'xhr-polling', 'iframe-xhr-polling', 'jsonp-polling'] })
+            //     this.stompClient = Stomp.over(this.websocket)
+            //     this.stompClient.heartbeat.outgoing = 0
+            //     this.stompClient.heartbeat.incoming = 0
+            //     // this.stompClient = Stomp.client(socketUrl)
+            //     this.stompClient.connect(
+            //         'guest',
+            //         'guest',
+            //         successCallback,
+            //         () => {
+            //             console.log('Oops! Reconnect')
+            //             setTimeout(() => {
+            //                 connectAndReconnect(socketUrl, successCallback)
+            //             }, 4000)
+            //         },
+            //         '/'
+            //     );
+            //
+            // }.bind(this)
+            // connectAndReconnect(socketUrl, successCallback)
         }
     }
 
     sendName(name) {
         console.log('Send')
-        // this.stompClient.send("/api/guestbook", {}, "Wow")
-        // this.stompClient.send('/queue/jsa.sendqueue', {'reply-to': '/temp-queue/foo'}, 'Wow')
-        // this.stompClient.send('/queue/jsa.sendqueue', {"content-type":"text/plain"}, 'Hello, STOMP')
-        // this.stompClient.send('/queue/jsa.sendqueue', {}, 'Hello, STOMP')
-        // 'Content-Type': 'application/json'
-        // text/html; charset=utf-8
-        this.stompClient.send('/queue/jsa.sendqueue', {}, JSON.stringify({'time':new Date().toTimeString(),'content':window.location.hostname}))
-        // this.stompClient.send('/queue/j  sa.sendqueue', {'reply-to': '/temp-queue/foo'}, {'time':new Date().toTimeString(),'content':'payload'})
-        // this.stompClient.send('/exchange/amq.direct/tenantname', {'reply-to': '/temp-queue/foo'}, 'Wow '+new Date().toTimeString())
-        // this.stompClient.send("/queue/jsa.queue", {}, "Wow")
     }
 
     componentDidUpdate() {
@@ -138,10 +127,10 @@ class TrainingApp extends Component {
         console.log('closing eventSource !')
         this.isTextSwitchAnimated = false
         // this.eventSource.close()
-        if(this.stompSubscription!=undefined )
-            this.stompSubscription.unsubscribe()
-        if(this.stompClient!=undefined )
-            this.stompClient.disconnect()
+        // if(this.stompSubscription!=undefined )
+        //     this.stompSubscription.unsubscribe()
+        // if(this.stompClient!=undefined )
+        //     this.stompClient.disconnect()
         // this.websocket.disconnect()
     }
 
