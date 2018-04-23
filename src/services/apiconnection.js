ffbcb47943eb018cee5b934f33e39e0f0154b8a0
Bensouda Rachid <bensourachid@gmail.com>
2018-04-23 15:21:00 +0100

    prepare an openshift working version without rabbitmq and with separate Rest api endpoint

--- src/services/apiconnection.js
+++ src/services/apiconnection.js
@@ -59,7 +59,8 @@ class ApiConnection {
             // authurl = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port
         } else {
             if (process.env.TRAINING_API_LOCAL_IP != undefined) {
-                authurl = 'http://' + process.env.TRAINING_API_LOCAL_IP + ':8080'
+                // authurl = 'http://' + process.env.TRAINING_API_LOCAL_IP + ':8080'
+                authurl = 'http://' + process.env.TRAINING_API_LOCAL_IP
             }
             else
                 authurl = undefined
